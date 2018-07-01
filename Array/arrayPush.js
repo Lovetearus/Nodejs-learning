1f0a229b22f822d5595cb24c8c941f5a7fba50b1
Lovetearus <Lovetearus@users.noreply.github.com>
2018-07-01 11:05:51 +0800

    7.1

--- Array/arrayPush.js
+++ Array/arrayPush.js
@@ -6,3 +6,17 @@ for (let i = 0; i < len; i++) {
     console.log(arr[i] === arrNew[i])
 }
 console.log(arrNew === arr)
+let vegetables = ["tomato", "potato", "parsnip"]
+let moreVeg = ["celery", "beetroot"]
+Array.prototype.push.apply(vegetables, moreVeg)
+console.log(vegetables)
+
+let obj = {
+    length: 0,
+    addElements: function(el) {
+        [].push.call(this, el)
+    }
+}
+obj.addElements({ x: 1 })
+obj.addElements({})
+console.log(obj)
